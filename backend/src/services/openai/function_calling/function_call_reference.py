2767a0e8d853a19637a9d913265c493fc2696785
bkmbigo <bkmbigo@gmail.com>
2024-09-30 12:52:01 +0300

    [backend]fix: remove references to medicine

--- backend/src/services/openai/function_calling/function_call_reference.py
+++ backend/src/services/openai/function_calling/function_call_reference.py
@@ -1,7 +1,5 @@
 from services.openai.function_calling.medical_center_function_calls import \
     get_closest_medical_centers_function_call, get_all_medical_centers_function_call, get_medical_center_function_call
-from services.openai.function_calling.medicine_function_calls import get_all_medicine_function_call, \
-    get_medicine_function_call, upsert_medicine_function_call
 from services.openai.function_calling.user_medication_function_calls import get_user_medications_function_call, \
     get_user_medication_function_call, upsert_user_medication_function_call, delete_user_medication_function_call
 
@@ -9,9 +7,6 @@ function_call_reference = {
     'get_closest_medical_centers_function_call': get_closest_medical_centers_function_call,
     'get_all_medical_centers_function_call': get_all_medical_centers_function_call,
     'get_medical_center_function_call': get_medical_center_function_call,
-    'get_all_medicine_function_call': get_all_medicine_function_call,
-    'get_medicine_function_call': get_medicine_function_call,
-    'upsert_medicine_function_call': upsert_medicine_function_call,
     'get_user_medications_function_call': get_user_medications_function_call,
     'get_user_medication_function_call': get_user_medication_function_call,
     'upsert_user_medication_function_call': upsert_user_medication_function_call,
